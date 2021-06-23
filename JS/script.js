4b21fe68b30abde23ad26e071723e1869e0604ad
Paulo Henrique Miranda Martins <paulolinhageral@yahoo.com.br>
2021-06-23 19:14:44 -0300

    Novo Commit

--- JS/script.js
+++ JS/script.js
@@ -32,3 +32,32 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+//TESTIMONIALS SWIPER SLIDER//
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+//Scroll REVEL//
+
+const scrollRevel = ScrollRevel({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollRevel.reveal(
+  `#home .image,  #home .text,
+  #about .image, #about .text,
+  #services header, #services . card,
+  #testimonials header, #testimonials .testimonials, 
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
