8c856e9cf7a4d84187f22c71c33098290f0021cf
Paulo Henrique Miranda Martins <paulolinhageral@yahoo.com.br>
2021-06-24 16:20:37 -0300

    FOOTER

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-/*Toggle MENU*/
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -8,9 +8,8 @@ for (const element of toggle) {
   })
 }
 
-/* Fechar menu*/
-
-const links = document.querySelectorAll('nav ul li a ')
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
   link.addEventListener('click', function () {
@@ -18,22 +17,21 @@ for (const link of links) {
   })
 }
 
-//Mudar o HEADER da pagina no Scroll//
+/* mudar o header da página quando der scroll */
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function () {
-  if ((window.scrollY = navHeight)) {
-    //scroll é maior que a altura do header
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
     header.classList.add('scroll')
   } else {
-    //menor que a altura do header
+    // menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
-//TESTIMONIALS SWIPER SLIDER//
+/* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
@@ -43,21 +41,38 @@ const swiper = new Swiper('.swiper-container', {
   keyboard: true
 })
 
-//Scroll REVEL//
-
-const scrollRevel = ScrollRevel({
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
   duration: 700,
   reset: true
 })
 
-scrollRevel.reveal(
-  `#home .image,  #home .text,
+scrollReveal.reveal(
+  `#home .image, #home .text,
   #about .image, #about .text,
-  #services header, #services . card,
-  #testimonials header, #testimonials .testimonials, 
-  #contact .text, #contact .links
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+/* Botão voltar para o topo */
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
